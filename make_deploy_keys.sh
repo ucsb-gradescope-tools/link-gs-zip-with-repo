e9bf9124dd57dc6e260c0bf3b5ca75641485d67a
Phill Conrad <pconrad@cs.ucsb.edu>
2018-04-16 19:23:23 -0700

    Update make_deploy_keys.sh

--- make_deploy_keys.sh
+++ make_deploy_keys.sh
@@ -2,3 +2,6 @@
 
 mkdir -p deploy_keys
 ssh-keygen -b 2048 -t rsa -f ./deploy_keys/deploy_key -q -N ""
+echo "*** Here is the public key that you should add as a deploy key"
+echo "***  for your repo:"
+cat ./deploy_keys/deploy_key.pub
